b2fe04be55aa0d88d4b0f0693bdfd330da7580de
unknown <farhandzaky679@gmail.com>
2024-05-15 10:11:25 +0700

    Perubahan pada login-page dan menghapus file css

--- Exe-File/login-exe.php
+++ Exe-File/login-exe.php
@@ -8,15 +8,17 @@
     $sql_user = mysqli_query($mysqli, "SELECT * FROM pengguna WHERE username='$username' AND password='$password'");
     $cek_user = mysqli_num_rows($sql_user);
 
-    if($cek_user > 0) {
-        if(stripos($username, "admin") !== false) {
+    if ($cek_user > 0) {
+        if (stripos($username, "admin") !== false) {
             header('Location: ../admin-dashboard.php');
             exit();
-        } else if(stripos($username, "manajer") !== false) {
+        } else if (stripos($username, "manajer") !== false) {
             header('Location: ../manajer-dashboard.php');
             exit();
         }
     } else {
+        $_SESSION['error'] = true;
         header('Location: ../login-page.php');
         exit();
     }
+?>
--- login-page.php
+++ login-page.php
@@ -1,3 +1,7 @@
+<?php
+    session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -14,13 +18,46 @@
     <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
-    <!-- External CSS -->
-    <link rel="stylesheet" href="Assets/style.css">
-
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     <style>
+        .login-branding {
+            padding-top: 5%;
+            padding-left: 2.5%;
+            height: 95vh;
+        }
+
+        .form-session {
+            margin-top: auto;
+            margin-bottom: auto;
+            padding-right: 13vh;
+        }
+
+        .login-welcome {
+            font-family: "Inter", sans-serif;
+            font-style: normal;
+            font-weight: 900;
+            font-size: 40px;
+            line-height: 48px;
+
+            color: #0077B6;
+        }
+
+        .login-text {
+            font-family: "Urbanist", sans-serif;
+            font-style: normal;
+            font-weight: 400;
+            font-size: 20px;
+            line-height: 140%;
+
+            color: #4F4F4F;
+        }
+
+        .login-form {
+            padding: 0 19%
+        }
+
         .form-control::placeholder {
             color: #7d7d7d;
         }
@@ -33,6 +70,29 @@
         .form-control:focus {
             box-shadow: none;
             border-color: #7d7d7d;
+        }
+
+        .wrapper {
+            width: 100%;
+            height: 140px;
+        }
+
+        .login-failed p {
+            color: red;
+        }
+
+        .login-btn {
+            height: 60px;
+            width: 100%;
+
+            background-color: #0077B6;
+
+            font-family: "Urbanist", sans-serif;
+            font-style: normal;
+            font-weight: 700;
+            font-size: 20px;
+
+            color: #FFFFFF;
         }
     </style>
 </head>
@@ -51,16 +111,28 @@
                         <label class="form-label login-text" for="username">Username</label>
                         <div class="input-group">
                             <span class="input-group-text" style="background-color: transparent; padding-left: 15px; border-right: none" id="basic-addon1"><i class="fa-solid fa-user" style="color: #7d7d7d"></i></span>
-                            <input type="text" id="username" name="username" class="form-control pt-3 pb-3" style="border-left: none; padding-left: 0px" placeholder="Masukkan nama pengguna anda" aria-label="Username" aria-describedby="basic-addon1">
+                            <input type="text" id="username" name="username" class="form-control pt-3 pb-3" style="border-left: none; padding-left: 0px" placeholder="Masukkan nama pengguna anda" aria-label="Username" aria-describedby="basic-addon1" required>
                         </div>
                     </div>
 
-                    <div style="margin-bottom: 68px">
+                    <div class="wrapper mb-4">
                         <label class="form-label login-text" for="password">Password</label>
                         <div class="input-group">
-                            <span class="input-group-text pt-3 pb-3" style="background-color: transparent; padding-left: 15px; border-right: none" id="basic-addon2"><i class="fa-solid fa-lock" style="color: #7d7d7d"></i></span>
-                            <input type="password" id="password" name="password" class="form-control pt-3 pb-3" style="border-left: none; padding-left: 0px;" placeholder="Masukkan kata sandi anda" aria-label="Password" aria-describedby="basic-addon2">
+                            <span class="input-group-text pt-3 pb-3 mb-2" style="background-color: transparent; padding-left: 15px; border-right: none" id="basic-addon2"><i class="fa-solid fa-lock" style="color: #7d7d7d"></i></span>
+                            <input type="password" id="password" name="password" class="form-control pt-3 pb-3 mb-2" style="border-left: none; padding-left: 0px;" placeholder="Masukkan kata sandi anda" aria-label="Password" aria-describedby="basic-addon2" required>
                         </div>
+
+                        <?php 
+                            if(isset($_SESSION['error'])):
+                        ?>
+                            <div class="login-failed">
+                                <p>Username atau Password Salah!</p>
+                                
+                            </div>                        
+                        <?php 
+                            unset($_SESSION['error']);
+                            endif; 
+                        ?>
                     </div>
 
                     <button type="submit" class="btn btn-primary login-btn">MASUK</button>
